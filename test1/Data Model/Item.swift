d72fff50e3ffd2b48eeae128e4f2615a8277e788
Swarnabh Agrawal <swarnabhagrawal@gmail.com>
2018-12-05 15:54:45 +0530

    Convert to MVC Design pattern.

---
+++ test1/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  test1
+//
+//  Created by BossmediaNT on 05/12/18.
+//  Copyright © 2018 swarnabh. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title:String=""
+    var done:Bool=false
+}
